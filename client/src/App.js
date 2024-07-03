26fb4dd48c47c2269114fe861d01c4e89974f816
Connor Verra <connorverra@gmail.com>
2024-07-03 17:04:48 -0400

    Added "container" div to allow for multiple elements

--- client/src/App.js
+++ client/src/App.js
@@ -20,39 +20,42 @@ function App() {
       </header>
     </div>
        */
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-          <a className="navbar-brand mx-2" href="#"> <img src={`${process.env.PUBLIC_URL}/images/calendar-week.svg`} height="30"/> Fake Company Name</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">About</a>
-              </li>
-              <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Roles
-                </a>
-                <ul className="dropdown-menu">
-                  <li><a className="dropdown-item" href="#">User</a></li>
-                  <li><a className="dropdown-item" href="#">Technician</a></li>
-                  <li><hr className="dropdown-divider"/></li>
-                  <li><a className="dropdown-item" href="#">Administrator</a></li>
-                </ul>
-              </li>
-            </ul>
-            <form className="d-flex" role="search">
-              <button className="btn btn-primary mx-2" type="submit">Sign In</button>
-              <button className="btn btn-outline-primary mx-2" type="submit">Sign Up</button>
-            </form>
+      <div className="container">
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+          <div className="container-fluid">
+            <a className="navbar-brand mx-2" href="#"> <img src={`${process.env.PUBLIC_URL}/images/calendar-week.svg`} height="30"/> Fake Company Name</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">About</a>
+                </li>
+                <li className="nav-item dropdown">
+                  <a className="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Roles
+                  </a>
+                  <ul className="dropdown-menu">
+                    <li><a className="dropdown-item" href="#">User</a></li>
+                    <li><a className="dropdown-item" href="#">Technician</a></li>
+                    <li><hr className="dropdown-divider"/></li>
+                    <li><a className="dropdown-item" href="#">Administrator</a></li>
+                  </ul>
+                </li>
+              </ul>
+              <form className="d-flex" role="search">
+                <button className="btn btn-primary mx-2" type="submit">Sign In</button>
+                <button className="btn btn-outline-primary mx-2" type="submit">Sign Up</button>
+              </form>
+            </div>
           </div>
-        </div>
-      </nav>
+        </nav>
+
+      </div>
   );
 }
 
