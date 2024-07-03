1973605a11fb8f6a078b3ace9499947ab95ef41f
Connor Verra <connorverra@gmail.com>
2024-07-03 17:24:42 -0400

    Added hero element to home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import logo from "./logo.svg";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
 
 function App() {
@@ -49,12 +50,30 @@ function App() {
               </ul>
               <form className="d-flex" role="search">
                 <button className="btn btn-primary mx-2" type="submit">Sign In</button>
-                <button className="btn btn-outline-primary mx-2" type="submit">Sign Up</button>
+                <button className="btn btn-outline-secondary mx-2" type="submit">Sign Up</button>
               </form>
             </div>
           </div>
         </nav>
 
+        <div className="container col-xxl-8 px-4 py-5">
+          <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
+            <div className="col-10 col-sm-8 col-lg-6">
+              <img src="bootstrap-themes.png" className="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy"/>
+            </div>
+            <div className="col-lg-6">
+              <h1 className="display-5 fw-bold text-body-emphasis lh-1 mb-3">The IT Help Desk Solution You Need</h1>
+              <p className="lead">Forget about manually scheduling appointments for your users. Our system will automatically handle the scheduling and assigning of appointments to
+                your employees based on the needs of your users, as well as including a host of additional features including live chat, powerful administrative privileges, and an
+                easy to user dashboard for all of your employees.
+                </p>
+                <div className="d-grid gap-2 d-md-flex justify-content-md-start">
+                <button type="button" className="btn btn-primary btn-lg px-4 me-md-2">Sign In</button>
+                <button type="button" className="btn btn-outline-secondary btn-lg px-4">Sign Up</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
   );
 }
