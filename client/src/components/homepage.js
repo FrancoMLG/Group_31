8dceb5b4d8fccce9d4fedb64a0699e4300c91fb4
Connor Verra <connorverra@gmail.com>
2024-07-04 18:04:42 -0400

    fixed some stuff

--- client/src/components/homepage.js
+++ client/src/components/homepage.js
@@ -1,5 +1,6 @@
-// import logo from "./logo.svg";
+
 import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import "../App.css";
 import {useNavigate} from "react-router-dom";
 
@@ -8,83 +9,42 @@ export default function Homepage() {
 
   return (
     <div className="container-fluid">
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-          <a className="navbar-brand mx-2" href="#">
-            <img
-              src={`${process.env.PUBLIC_URL}/images/calendar-week.svg`}
-              height="30"
-            />
-            Fake Company Name
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  About
-                </a>
-              </li>
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false">
-                  Roles
-                </a>
-                <ul className="dropdown-menu">
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      User
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Technician
-                    </a>
-                  </li>
-                  <li>
-                    <hr className="dropdown-divider" />
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Administrator
-                    </a>
-                  </li>
-                </ul>
-              </li>
-            </ul>
-            <form className="d-flex" role="search">
-              <button className="btn btn-primary mx-2" type="submit">
-                Sign In
-              </button>
-              <button
-                className="btn btn-outline-secondary mx-2"
-                type="submit"
-                onClick={() => navigate("/signup")}>
-                Sign Up
-              </button>
-            </form>
+      <nav className="navbar navbar-expand-lg fixed-top bg-body-tertiary">
+          <div className="container-fluid">
+            <a className="navbar-brand mx-2" href="#"> <img src={`${process.env.PUBLIC_URL}/images/calendar-week.svg`} height="30"/> Fake Company Name</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">About</a>
+                </li>
+                <li className="nav-item dropdown">
+                  <a className="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Roles
+                  </a>
+                  <ul className="dropdown-menu">
+                    <li><a className="dropdown-item" href="#">User</a></li>
+                    <li><a className="dropdown-item" href="#">Technician</a></li>
+                    <li><hr className="dropdown-divider"/></li>
+                    <li><a className="dropdown-item" href="#">Administrator</a></li>
+                  </ul>
+                </li>
+              </ul>
+              <form className="d-flex" role="search">
+                <button className="btn btn-primary mx-2" type="submit">Sign In</button>
+                <button className="btn btn-outline-secondary" type="submit" onClick={() => navigate("/signup")}>Sign Up</button>
+              </form>
+            </div>
           </div>
-        </div>
-      </nav>
+        </nav>
 
-      <div className="container col-xxl-8 px-4 py-5">
+
+      <div className="container col-xxl-8 px-2 py-5">
         <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
           <div className="col-10 col-sm-8 col-lg-6">
             // fixme: add a better image, maybe of finished product?
