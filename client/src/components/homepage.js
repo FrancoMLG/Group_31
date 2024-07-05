c4a4493835b07b6688b741534bd7f06709cffa1c
FrancoMLG <hutchinson.frank@ufl.edu>
2024-07-05 12:49:00 -0400

    Added new image to homepage

--- client/src/components/homepage.js
+++ client/src/components/homepage.js
@@ -47,9 +47,8 @@ export default function Homepage() {
       <div className="container col-xxl-8 px-2 py-5">
         <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
           <div className="col-10 col-sm-8 col-lg-6">
-            // fixme: add a better image, maybe of finished product?
             <img
-              src={`${process.env.PUBLIC_URL}/images/calendar-week.svg`}
+              src={`${process.env.PUBLIC_URL}/images/it_help_desk.svg`}
               className="d-block mx-lg-auto img-fluid"
               alt="Bootstrap Themes"
               width="700"
