4de239b72d49fda86744ddf455bbe6b3f40d0c8c
Peyton Kromash <pkromash@gmail.com>
2024-07-07 11:22:05 -0400

    Abstracted Navbar into its own component and added authentication checking to navbar.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Homepage from "./components/homepage";
 import SignupForm from "./components/Auth/signup";
-import SigninForm from "./components/Auth/signin"
+import SigninForm from "./components/Auth/signin";
+import Navbar from "./components/navbar";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/signup" element={<SignupForm />} />
