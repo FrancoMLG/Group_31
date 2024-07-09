753ed8e81d464064cf2cf90b1fbc2db3339954b8
Connor Verra <connorverra@gmail.com>
2024-07-09 13:29:19 -0400

    added function for modifying appearance

--- client/src/components/dashboards/user/userhome.js
+++ client/src/components/dashboards/user/userhome.js
@@ -5,10 +5,18 @@ import React from "react";
 import UserSideBar from "./user_sidebar";
 import "./userhome.css"
 
-export default function Userhome() {
+
+function setActiveLink() {
+    document.getQuerySelector("UserSidebar #home-link").classList.toggle("active");
+    document.getQuerySelector("UserSidebar #home-link").classList.toggle('aria-current="page"');
+}
+
+export default function UserHome() {
   // const navigate = useNavigate();
 
   const signedIn = localStorage.getItem("profile");
+
+  // <a href="#" className="nav-link active" aria-current="page"> code for setting active part of dashboard?
 
   return (
     <div className="container-fluid bg-body-tertiary vh-100 d-flex flex-column no-padding">
@@ -16,5 +24,8 @@ export default function Userhome() {
       <UserSideBar />
 
     </div>
+
   );
+
+
 }
