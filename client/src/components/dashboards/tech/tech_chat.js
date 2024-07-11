d921ae858f6e79d200704135de3fbc18866b9cc1
FrancoMLG <hutchinson.frank@ufl.edu>
2024-07-11 00:15:46 -0400

    Set up tech chat

--- client/src/components/dashboards/tech/tech_chat.js
+++ client/src/components/dashboards/tech/tech_chat.js
@@ -14,9 +14,12 @@ export default function TechChat() {
           <TechSideBar activeLinkId={activeLinkId} />
         </div>
         <div className="col overflow-auto">
-
-          <p>Insert More Content Here!</p>
-
+		<br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+			<br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+		  <form class="chat-input-form">
+		  <input type="text" class="chat-input" required placeholder="Type here"/>
+		  <button type="submit" class="button send-button">Send</button>
+		  </form>
         </div>
       </div>
     </div>
