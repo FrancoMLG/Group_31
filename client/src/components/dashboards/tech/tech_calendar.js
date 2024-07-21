d082d21554264037b6bd827ca3b1bb366982cceb
xiaoguo-jia <xiaoguojia@ufl.edu>
2024-07-21 16:01:05 -0400

    integrate calendar into tech dashboard

--- client/src/components/dashboards/tech/tech_calendar.js
+++ client/src/components/dashboards/tech/tech_calendar.js
@@ -3,6 +3,7 @@ import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import { Link, useNavigate } from "react-router-dom";
 import React from "react";
 import TechSideBar from "./tech_sidebar";
+import Calendar from "../../calendar/Calendar";
 
 export default function TechCalendar() {
   const activeLinkId = "calendar-link";
@@ -14,9 +15,9 @@ export default function TechCalendar() {
           <TechSideBar activeLinkId={activeLinkId} />
         </div>
         <div className="col overflow-auto">
+          <Calendar />
 
           <p>Insert More Content Here!</p>
-
         </div>
       </div>
     </div>
