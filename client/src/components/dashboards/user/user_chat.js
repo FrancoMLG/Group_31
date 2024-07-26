cf0be574d7d79112b5bf32777770fb2d85d15c33
FrancoMLG <hutchinson.frank@ufl.edu>
2024-07-26 19:47:11 -0400

    Added basic chat function

--- client/src/components/dashboards/user/user_chat.js
+++ client/src/components/dashboards/user/user_chat.js
@@ -1,13 +1,22 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import { Link, useNavigate } from "react-router-dom";
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import UserSideBar from "./user_sidebar";
 import "./user_chat.css";
 
 export default function UserChat() {
   const activeLinkId = "chat-link";
-
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+  const messagesEndRef = useRef(null);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+	setMessages([...messages, { text: message }]);
+    setMessage("");
+	messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+  };
+  
   return (
     <div className="container-fluid bg-body-tertiary vh-100 d-flex flex-column no-padding">
       <div className="row flex-grow-1">
@@ -17,8 +26,16 @@ export default function UserChat() {
         <div className="col overflow-auto">
 			<br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
 			<br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
-		  <form class="chat-input-form">
-		  <input type="text" class="chat-input" required placeholder="Type here"/>
+			<div className="messages-container">
+            {messages.map((msg, index) => (
+              <div key={index} className="chatMessage blue-bg-body-tertiary">
+                <div className="message-text">{msg.text}</div>
+              </div>
+            ))}
+            <div ref={messagesEndRef}/>
+          </div>
+		  <form id="chatForm" class="chat-input-form" onSubmit={handleSubmit}>
+		  <input type="text" class="chat-input" value={message} onChange={(e) => setMessage(e.target.value)} required placeholder="Type here"/> 
 		  <button type="submit" class="button send-button">Send</button>
 		  </form>
         </div>
